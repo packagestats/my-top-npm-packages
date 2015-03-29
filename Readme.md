f0f5b60debe88685f351dc147fcf64bd1ea8f64b
Joel Kemp <mrjoelkemp@gmail.com>
2015-03-29 00:45:20 -0400

    Switch to the npm api

--- Readme.md
+++ Readme.md
@@ -10,19 +10,3 @@
 
 * Defaults to showing you the download counts in the last `month`.
 * Other options: `--week` or `--day`
-
-### How does it work
-
-Scrapes the npm profile of the specified user, pulls the stats for every one of their packages,
-then ranks them by the specified time period.
-
-I currently have about ~40 published packages and this takes ~19 seconds to finish.
-If you're [@sindresorhus](https://github.com/sindresorhus), go make some popcorn – it'll be a while.
-
-### Beware, this will break if
-
-* npm paginates the list of packages on a user's profile page
-* npm changes the html for the sidebar on the package's page
-* npm changes their url structure
-
-Scraping, ftw.
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var request = require('request');
 var q = require('q');
-var $ = require('jquery')(require('jsdom').jsdom().parentWindow);
+var Registry = require('npm-registry');
+var npm = new Registry();
+
+var listPackagesForUser = q.nbind(npm.users.list, npm.users);
+var getDownloadTotals = q.nbind(npm.downloads.totals, npm.downloads);
 
 function Package(options) {
   this.name = options.name || '';
-  this.url = options.url || '';
   this.counts = options.counts || null;
 }
 
@@ -16,118 +18,50 @@ function Package(options) {
 module.exports = function(username, cb, sortBy) {
   sortBy = sortBy || 'month';
 
-  getProfilePageForUser(username)
-  .then(getLinksForPackages)
-  .then(getCountsForPackages)
-  .then(sortCountsForPackages.bind(null, sortBy))
+  listPackagesForUser(username)
+  .then(getCommaSeparatedPackages)
+  .then(getCounts)
+  .then(sortCounts.bind(null, sortBy))
   .done(cb);
 };
 
-/**
- * @param  {String} username
- * @return {Promise} Resolves with the HTML content of that user's npm profile page
- */
-function getProfilePageForUser(username) {
-  var deferred = q.defer();
-
-  request('https://www.npmjs.com/~' + username, function(error, response, body) {
-    if (error) {
-      deferred.reject(error);
-      return;
-    }
-
-    if (response.statusCode == 200) {
-      deferred.resolve(body);
-
-    } else {
-      deferred.reject();
-    }
-  });
-
-  return deferred.promise;
+function getCommaSeparatedPackages(packages) {
+  return packages.map(function(p) {
+    return p.name;
+  }).join(',');
 }
 
-/**
- * Grabs the list of all package links found on a user's npm profile page
- * @param  {String} profilePageHTML
- * @return {Object} - package name -> url hash
- */
-function getLinksForPackages(profilePageHTML) {
-  var $links = $(profilePageHTML).find('.content ul:first li a');
-  var host = 'https://www.npmjs.com';
-  var linkMap = {};
+function getCounts(commaSepNames) {
+  var packageNames = commaSepNames.split(',');
+  var packageStats = [];
 
-  $links.each(function(idx, el) {
-    var href = host + el.href.replace('file://', '');
-    // The text of the link is truncated, so we grab it from the url
-    var packageName = href.split('package/')[1];
-    linkMap[packageName] = host + el.href.replace('file://', '');
-  });
+  var lastDayStats;
+  var lastWeekStats;
+  var lastMonthStats;
 
-  return linkMap;
-}
+  return getDownloadTotals('last-day', commaSepNames)
+  .then(function(stats) {
+    lastDayStats = stats[0];
+    return getDownloadTotals('last-week', commaSepNames);
+  })
+  .then(function(stats) {
+    lastWeekStats = stats[0];
+    return getDownloadTotals('last-month', commaSepNames);
+  })
+  .then(function(stats) {
+    lastMonthStats = stats[0];
 
-/**
- * @param  {Object} linkMap - package name -> url hash
- * @return {Promise} - Resolves with a list of Packages
- */
-function getCountsForPackages(linkMap) {
-  var packages = Object.keys(linkMap);
-
-  return q.all(packages.map(function(packageName) {
-    var packageUrl = linkMap[packageName];
-
-    return getCountsForPackage(packageUrl)
-    .then(function(counts) {
+    return packageNames.map(function(name) {
       return new Package({
-        url: packageUrl,
-        name: packageName,
-        counts: counts
+        name: name,
+        counts: {
+          day: lastDayStats[name] ? lastDayStats[name].downloads : 0,
+          week: lastWeekStats[name] ? lastWeekStats[name].downloads : 0,
+          month: lastMonthStats[name] ? lastMonthStats[name].downloads : 0
+        }
       });
     });
-  }));
-}
-
-/**
- * @param  {String} packageUrl
- * @return {Promise} Resolves with an object containing the download counts for a single package
- */
-function getCountsForPackage(packageUrl) {
-  var deferred = q.defer();
-
-  request(packageUrl, function(error, response, body) {
-    var counts = {
-      day: null,
-      week: null,
-      month: null
-    };
-
-    if (error || response.statusCode !== 200) {
-      deferred.resolve(counts);
-      return;
-    }
-
-    var $stats = $(body).find('h3:contains("Stats")').next('ul').find('li').slice(0, 3);
-    var getCountForIndex = function(idx) {
-      try {
-        var text = $stats.get(idx).textContent;
-        var stripped = text.replace(',', '');
-        return parseInt(stripped, 10);
-      } catch(e) {
-        return 0;
-      }
-    };
-
-    var counts = {
-      day: getCountForIndex(0),
-      week: getCountForIndex(1),
-      month: getCountForIndex(2),
-    };
-
-    deferred.resolve(counts);
   });
-
-  return deferred.promise;
 }
 
 /**
@@ -136,7 +70,7 @@ function getCountsForPackage(packageUrl) {
  * @param  {Package[]} packages
  * @return {Package[]} Sorted list of Packages
  */
-function sortCountsForPackages(sortBy, packages) {
+function sortCounts(sortBy, packages) {
   return packages.sort(function(p1, p2) {
     return p2.counts[sortBy] - p1.counts[sortBy];
   });
